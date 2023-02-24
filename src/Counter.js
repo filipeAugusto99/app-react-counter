1b0ac745d06b6282cae93cc5ffa0249853443402
Filipe Augusto <filipecordeiro213@gmail.com>
2023-02-24 14:37:53 -0300

    cicle of life (use effects)

--- src/Counter.js
+++ src/Counter.js
@@ -1,9 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import './Counter.css'
 
+// useEffect
+
 const Counter = () => {
   const [counter, setCounter] = useState(0)
+  const [test, setTest] = useState('teste')
+
+  useEffect(() => {
+    console.log('estado alterado')
+  }, [test])
+
+  useEffect(() => {
+    console.log('renderizando contador')
+  }, [counter])
 
   const handleOnClick = operator => {
     let newValue = operator === '+' 
@@ -19,6 +30,7 @@ const Counter = () => {
         <span>{counter}</span>
         <button onClick={() => handleOnClick('-')}>-</button>
         <button onClick={() => handleOnClick('+')}>+</button>
+        <button onClick={() => setTest('otherValue')}>TESTE</button>
       </div>
     </div>
   )
